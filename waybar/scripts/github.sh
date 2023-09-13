7ba723689a69da376076730919a9f39e9ad5df4b
command-z-z <272181284@qq.com>
2023-09-13 10:13:39 +0800

    waybar: add spotify config control

--- waybar/scripts/github.sh
+++ waybar/scripts/github.sh
@@ -3,5 +3,6 @@
 NOTIFICATIONS="$(gh api notifications)"
 COUNT="$(echo "$NOTIFICATIONS" | jq 'length')"
 
-# echo '{"text": '$COUNT',"tooltip": "'$COUNT' Notifications"}'
 echo '{"text": 0,"tooltip": "0 Notifications"}'
+# echo "{\"text\":$COUNT,\"tooltip\":\"$COUNT Notifications\",\"class\":\"\"}"
+
