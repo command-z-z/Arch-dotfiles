1234d8c638af7f66793067e8a0eb3df6dd32ee0d
command-z-z <272181284@qq.com>
2023-10-08 16:48:08 +0800

    neovim: upgrade indent-blankline to version3

--- nvim/after/plugin/blankline.lua
+++ nvim/after/plugin/blankline.lua
@@ -1,47 +1,35 @@
-local status, blankline = pcall(require, "indent_blankline")
+local status, blankline = pcall(require, "ibl")
 if (not status) then
     return
 end
 
-blankline.setup({
-    char = '│',
-    use_treesitter_scope = true,
-    show_first_indent_level = true,
-    show_current_context = false,
-    show_current_context_start = false,
-    show_current_context_start_on_current_line = false,
-    filetype_exclude = {
-        'dashboard',
-        'DogicPrompt',
-        'log',
-        'fugitive',
-        'gitcommit',
-        'packer',
-        'markdown',
-        'json',
-        'txt',
-        'vista',
-        'help',
-        'todoist',
-        'NvimTree',
-        'git',
-        'TelescopePrompt',
-        'undotree',
-        'defx',
+blankline.setup {
+    indent = { char = "│" },
+    exclude = {
+        filetypes = {
+            'dashboard',
+            'DogicPrompt',
+            'log',
+            'fugitive',
+            'gitcommit',
+            'packer',
+            'markdown',
+            'json',
+            'txt',
+            'vista',
+            'help',
+            'todoist',
+            'NvimTree',
+            'git',
+            'TelescopePrompt',
+            'undotree',
+            'defx',
+        },
+        buftypes = {
+            'terminal', 'nofile', 'prompt'
+        },
     },
-    buftype_exclude = { 'terminal', 'nofile', 'prompt' },
-    context_patterns = {
-        'class',
-        'function',
-        'method',
-        'block',
-        'list_literal',
-        'selector',
-        '^if',
-        '^table',
-        'if_statement',
-        'while',
-        'for',
+    scope = {
+       enabled = false,
     },
-})
-
+}
